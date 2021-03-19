173131a32e76dd843789a4ae6ce894cb16c24bbb
minhphanhvu <minhphanh.vu@gmail.com>
2021-03-19 14:28:52 -0500

    -Add test for requiring the right user for edit and update actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,4 +40,9 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+
 end
