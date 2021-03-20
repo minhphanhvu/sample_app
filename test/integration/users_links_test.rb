500414ac4a52979a1ed541c240ff25a5bb5ffff9
minhphanhvu <minhphanh.vu@gmail.com>
2021-03-20 11:30:37 -0500

    Add integration tests for logged-in and non-loggedin-in users layout links

---
+++ test/integration/users_links_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersLinksTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layouts as non-logged-in users" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", login_path, count: 1
+  end
+
+  test "layouts as logged-in users" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", login_path, count: 0
+    get users_path
+    assert_template 'users/index'
+    assert_select "img", count: 2
+  end
+
+end
