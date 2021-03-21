5a3cae064a901caa860d0394c0728e972e5600a6
minhphanhvu <minhphanh.vu@gmail.com>
2021-03-20 21:40:12 -0500

    Email tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
 
     def create_activation_digest
       self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)  
+      self.activation_digest = User.digest(activation_token)
     end
 
 end
