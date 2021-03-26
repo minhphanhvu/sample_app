3738f9eba35fd66e5f2c7034b5820e2510217e26
minhphanhvu <minhphanh.vu@gmail.com>
2021-03-26 14:36:57 -0500

    Give access to logged_in_user to microposts controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ require 'byebug'
 
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+    # Confirm a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
