b1995e6c276db432243e944bbd97b524c561a477
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-04 13:04:41 -0500

    Add Stats and follow + unfollow forms

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,13 @@ Rails.application.routes.draw do
   get 'static_pages/contact', as: 'contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]    
 end
