81befc103aa7566e611acff32e1c46b904182e16
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-05 23:21:44 -0500

    Feed implemntation (with following_ids)

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,7 +77,7 @@ class User < ApplicationRecord
 
   # Display feed on home page
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
   # Follows a user.
