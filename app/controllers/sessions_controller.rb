38ba7b4ce2e6db34b3b80c7157d34b36b76f083e
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-16 22:17:27 -0500

    Testing remember_token attribute in integration test "login without remembering"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      if user.activated?
+      if @user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
-        log_in(user)
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or forwarding_url || user
+        log_in(@user)
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or forwarding_url || @user
       else
         message = "Account not activated. "
         message += "Check your email for the activation link."
